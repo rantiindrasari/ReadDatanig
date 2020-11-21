8808bf2b24f3ac9733388efdef0052ea6961fd2b
FlutLab.io <>
2020-11-21 09:49:01 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,155 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<Guru>> fetchGurus(http.Client client) async {
+  final response =
+      await client.get('https://rantiindrasari.000webhostapp.com/readDatanig.php');
+
+  // Use the compute function to run parseGurus in a separate isolate.
+  return compute(parseGurus, response.body);
+}
+
+// A function that converts a response body into a List<Guru>.
+List<Guru> parseGurus(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsed.map<Guru>((json) => Guru.fromJson(json)).toList();
+}
+
+class Guru {
+  final String nig;
+  final String nama_guru;
+  final String jenjang_akademik;
+  final String pendidikan_terakhir;
+  final String home_base;
+
+  Guru({this.nig, this.nama_guru, this.jenjang_akademik, this.pendidikan_terakhir, this.home_base});
+
+  factory Guru.fromJson(Map<String, dynamic> json) {
+    return Guru(
+      nig: json['nig'] as String,
+      nama_guru: json['nama_guru'] as String,
+      jenjang_akademik: json['jenjang_akademik'] as String,
+      pendidikan_terakhir: json['pendidikan_terkakhir'] as String,
+      home_base: json['home_base'] as String,
+    );
+  }
+}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appTitle = 'Data Guru';
+
+    return MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    ); 
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: FutureBuilder<List<Guru>>(
+        future: fetchGurus(http.Client()),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
+
+          return snapshot.hasData
+              ? GurusList(GuruData: snapshot.data)
+              : Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+}
+
+class GurusList extends StatelessWidget {
+  final List<Guru> GuruData;
+
+  GurusList({Key key, this.GuruData}) : super(key: key);
+
+
+
+Widget viewData(var data,int index)
+{
+return Container(
+    width: 200,
+    child: Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      color: Colors.green,
+      elevation: 10,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+            //ClipRRect(
+              //      borderRadius: BorderRadius.only(
+                //      topLeft: Radius.circular(8.0),
+                  //    topRight: Radius.circular(8.0),
+                   // ),
+                   // child: Image.network(
+                    //    "https://elearning.binadarma.ac.id/pluginfile.php/1/theme_lambda/logo/1602057627/ubd_logo.png"
+                    //    width: 100,
+                     //   height: 50,
+                        //fit:BoxFit.fill
+
+                   // ),
+                 // ),
+            
+          ListTile(
+           //leading: Image.network(
+             //   "https://elearning.binadarma.ac.id/pluginfile.php/1/theme_lambda/logo/1602057627/ubd_logo.png",
+             // ),
+            title: Text(data[index].nig, style: TextStyle(color: Colors.white)),
+            subtitle: Text(data[index].nama_guru, style: TextStyle(color: Colors.white)),
+          ),
+          ButtonTheme.bar(
+            child: ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child: const Text('Edit', style: TextStyle(color: Colors.white)),
+                  onPressed: () {},
+                ),
+                FlatButton(
+                  child: const Text('Delete', style: TextStyle(color: Colors.white)),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemCount: GuruData.length,
+      itemBuilder: (context, index) {
+        return viewData(GuruData,index);
+      },
+    );
+  }
+}
